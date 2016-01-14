a6ca740bbdb8624f804d3b7b786863d026a515c1
rang1 <rang1@asu.edu>
2016-01-13 19:16:39 -0700

    encrypt and decrypt works

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -21,12 +21,77 @@ The program is written on MS Visual Studio C++ platform, Windows x64
 #include <sstream>
 #include <vector>
 
+std::string encrypt(std::string &iostr, int key);//http://www.cplusplus.com/forum/beginner/115317/ 
+std::string decrypt(std::string &iostr, int key);
+void doIt();//actual main
+
 int main()
 {
 	//_CrtSetBreakAlloc(307); //memory leak reported ith memory allocation. This will break at ith.
-	//appStore();
+	doIt();
 #ifdef _WIN32 
 	_CrtDumpMemoryLeaks();//checks for memory leak. MUST USE DEBUGGER
 #endif
 	return 0;
+}
+void doIt()
+{
+	std::string source;
+	std::cout << "Enter a message" << std::endl;
+	std::cin >> source;
+
+	int key;//move by 15
+	std::cout << "Enter an integer" << std::endl;
+	std::cin >> key;
+
+
+	std::string en = encrypt(source, key);
+
+	std::cout << "encrypted: " << en << std::endl;
+
+	std::string de = decrypt(en, key);
+
+	std::cout << "decrypted: " << de << std::endl;
+}
+
+
+std::string encrypt(std::string &iostr, int key)
+{
+	key %= 26;//take key and divide by 26 and take remainder
+	int ch;
+	std::string result = iostr;
+
+	for (auto &it : result)
+	{
+		ch = tolower(it) + key;
+
+		if (key < 0 && ch < 'a')
+			ch += 26;
+
+		if (ch > 'z')
+			ch -= 26;
+		it = ch;
+	}
+	return result;
+
+}
+
+std::string decrypt(std::string &iostr, int key)
+{
+	key %= 26;//take key and divide by 26 and take remainder
+	int ch;
+	std::string result = iostr;
+
+	for (auto &it : result)
+	{
+		ch = tolower(it) - key;
+
+		if (key < 0 && ch > 'z')
+			ch -= 26;
+
+		if (ch < 'a')
+			ch += 26;
+		it = ch;
+	}
+	return result;
 }
